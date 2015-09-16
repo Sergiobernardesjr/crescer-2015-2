0f79c775d27635cecc2a69cc6b61741fdd55bfa5
Sergio Luiz Nunes Bernardes Junior <sergio.bernardes@hotmail.com.br>
2015-09-16 17:32:52 -0300

    Final_aula_16092015

--- Exercicios_elfo/Dwarf.java
+++ Exercicios_elfo/Dwarf.java
@@ -1,10 +1,13 @@
 
 public class Dwarf
 {
-    private int vida = 110;   
+    private int vida = 115;   
         
     public void danoRecebido(){
         this.vida -= 10;
     }
     
+    public int getVida(){
+        return this.vida;
+    }
 }
---
+++ Exercicios_elfo/DwarfTest.java
@@ -0,0 +1,15 @@
+
+
+import static org.junit.Assert.*;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class DwarfTest
+{
+    @Test
+    public void quandoDwarfNasceCom110DeVida(){
+        Dwarf gimli = new Dwarf();
+        assertEquals(110, gimli.getVida());
+    }
+}
