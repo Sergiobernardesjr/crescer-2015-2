20d280b075584d3c00ea3e5ace96282fdf5d4641
Sergio Luiz Nunes Bernardes Junior <sergio.bernardes@hotmail.com.br>
2015-09-23 18:38:12 -0300

    Teste para NightElf.

--- Exercicios_elfo/NightElf.java
+++ Exercicios_elfo/NightElf.java
@@ -13,8 +13,8 @@ public class NightElf extends Elfo{
     public void atirarFlecha(Dwarf dwarf){
         super.atirarFlecha(dwarf);
         this.experiencia += 2;
-        double vidaPerdida = this.vida * 0.05;
-        this.vida -= (int)vidaPerdida;
+        double vidaPerdida = this.vida * 0.95;
+        this.vida = (int)vidaPerdida;
         this.status = this.vida == 0 ? Status.MORTO : this.status;
     }  
 }
--- Exercicios_elfo/NightElfTest.java
+++ Exercicios_elfo/NightElfTest.java
@@ -8,7 +8,7 @@ public class NightElfTest
 {
     @Test
     public void nightElfAtacaDwarfEGanhaTriploDeExperienciaEPerdeVida(){
-        NightElf elfo = new NightElf("Elfo");
+        Elfo elfo = new NightElf("Elfo");
         Dwarf dwarf = new Dwarf("dwarf");
         int flechasEsperada = 41, 
         experienciaEsperada = 3;
@@ -23,8 +23,8 @@ public class NightElfTest
 
     @Test
     public void elfoAtacaIrishDwarfEGanhaTriploDeExperiencia(){
-        NightElf elfo = new NightElf("Elfo");
-        IrishDwarf dwarf = new IrishDwarf("dwarf");
+        Elfo elfo = new NightElf("Elfo");
+        Dwarf dwarf = new IrishDwarf("dwarf");
         int flechasEsperada = 41, 
         experienciaEsperada = 3;
         double vidaEsperada = 95;
@@ -36,4 +36,44 @@ public class NightElfTest
         assertEquals(vidaEsperada, elfo.getVida(),0.00001);
     }   
 
+    @Test
+    public void elfoAtacaDwarfEIrishDwarfEGanha6DeExperienciaEPerdeVidaPorAtaque(){
+        Elfo elfo = new NightElf("Elfo");
+        Dwarf dwarf = new IrishDwarf("Dwarf");
+        Dwarf dwarf2 = new Dwarf("Dwarfx");
+        
+        int flechasEsperada = 40,
+        experienciaEsperada = 6;
+        double vidaEsperada = 0,
+        vida1,
+        vidaPerdida;
+        
+        vida1 = (100*0.95);
+        vidaPerdida = vida1 * 0.95;
+        vidaEsperada = (int)vidaPerdida;
+        
+        elfo.atirarFlecha(dwarf);
+        elfo.atirarFlecha(dwarf2);
+        
+        assertEquals(flechasEsperada, elfo.getFlechas());
+        assertEquals(vidaEsperada, elfo.getVida(),0.00001);
+        assertEquals(experienciaEsperada, elfo.getExperiencia());
+        assertEquals(Status.VIVO, elfo.getStatus());
+        
+    }
+    
+        @Test
+    public void elfoAtacaDwarfEMorre(){
+        Elfo elfo = new NightElf("Elfo");
+        Dwarf dwarf = new Dwarf("dwarf");
+        
+        double vidaEsperada = 0;
+        for(int i = 0; i <= 100; i++){
+            elfo.atirarFlecha(dwarf);
+        }
+        
+        assertEquals(vidaEsperada, elfo.getVida(),0.00001);
+        assertEquals(Status.MORTO, elfo.getStatus());
+        
+    }
 }
