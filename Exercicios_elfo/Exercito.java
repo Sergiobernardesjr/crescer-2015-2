6c37704ee72dba37d683a1044a2b2c169408296a
Sergio Luiz Nunes Bernardes Junior <sergio.bernardes@hotmail.com.br>
2015-09-23 18:57:07 -0300

    Criado class Exercito

---
+++ Exercicios_elfo/Exercito.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class Exercito{
+    private ArrayList<String> exercito = new ArrayList<>();
+    
+    public Exercito(){
+    }
+    
+    public void alistarElfo(Elfo elfo){
+       
+    }
+    
+}
