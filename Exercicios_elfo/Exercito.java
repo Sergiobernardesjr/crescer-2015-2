809b5685520808f53c0ab9d86f42a318a0cdb770
Sergio Luiz Nunes Bernardes Junior <sergio.bernardes@hotmail.com.br>
2015-09-24 11:37:50 -0300

    Incluído metodo hashmap no exercito.

--- Exercicios_elfo/Exercito.java
+++ Exercicios_elfo/Exercito.java
@@ -1,13 +1,18 @@
 import java.util.*;
 
 public class Exercito{
-    private ArrayList<String> exercito = new ArrayList<>();
+    private Map<Status, ArrayList<Elfo>> exercito = new HashMap<>();
     
     public Exercito(){
     }
     
     public void alistarElfo(Elfo elfo){
-       
-    }
-    
+        String nomeElfo;
+        boolean nightElfOuGreenElf = (elfo instanceof NightElf || elfo instanceof GreenElf) && elfo.getNome() != null;
+        
+        if (nightElfOuGreenElf){
+            exercito.put(elfo.getStatus(),new ArrayList<Elfo>());
+        }
+ 
+    }    
 }
