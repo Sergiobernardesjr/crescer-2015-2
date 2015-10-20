41fd872ae66acb20852b454608f88007a8286780
Sergio Luiz Nunes Bernardes Junior <sergio.bernardes@hotmail.com.br>
2015-10-20 18:35:56 -0200

    feat(carrinho-compras.js): Adicionado atualizado o nome do objeto Carrinho para CarrinhoDeCompras e atualizar o metodo removerItem.

--- src/Modulo_03_JS/Dia 3/carrinho-compras.js
+++ src/Modulo_03_JS/Dia 3/carrinho-compras.js
@@ -5,15 +5,15 @@ function Item (sku, descricao, quantidade, valorUnitario){
   this.valorUnitario = valorUnitario;
 };
 
-function Carrinho (){
+function CarrinhoDeCompras (){
   this.itens = new Array();
 };
 
-Carrinho.prototype.adicionarItem = function(item){
+CarrinhoDeCompras.prototype.adicionarItem = function(item){
   this.itens.push(item);
 };
 
-Carrinho.prototype.procurarItemPorSku = function(sku){
+CarrinhoDeCompras.prototype.procurarItemPorSku = function(sku){
   var indice = 0;
   var itemSku = this.itens.map(function(elem){
     return elem.sku;
@@ -22,7 +22,21 @@ Carrinho.prototype.procurarItemPorSku = function(sku){
   return indice
 };
 
-Carrinho.prototype.removerItem = function(sku, quantidade){
-  var itemASerRemovido = carrinho.procurarItemPorSku(sku);
-  this.itens.splice(itemASerRemovido, quantidade);
+CarrinhoDeCompras.prototype.removerItem = function(sku, quantidade){
+  var itemASerRemovido = this.procurarItemPorSku(sku);
+  if (this.itens[itemASerRemovido].quantidade > quantidade){
+    this.itens[itemASerRemovido].quantidade -= quantidade;
+  }
+  else if (this.itens[itemASerRemovido].quantidade === quantidade){
+    this.itens.splice(itemASerRemovido, quantidade);
+  }
+  else {
+    alert('Não é permitido remover item com a quantidade menor que existente no carrinho!');
+  }
+
+}
+
+CarrinhoDeCompras.prototype.atualizarQuantidade = function(sku, quantidade){
+  var itemASerAtualizado = this.procurarItemPorSku(sku);
+  this.itens[itemASerAtualizado].quantidade = quantidade;
 }
