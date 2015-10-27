9a50cd175f5a80d8aa18383256da7b74074f1152
Sergio Luiz Nunes Bernardes Junior <sergio.bernardes@hotmail.com.br>
2015-10-27 10:20:00 -0200

    feat(AgendaTestes.cs): adicionado testes unitários;

--- src/modulo-03-c-sharp/ConsoleApp/UnitTest1/AgendaTestes.cs
+++ src/modulo-03-c-sharp/ConsoleApp/UnitTest1/AgendaTestes.cs
@@ -11,13 +11,74 @@ namespace ConsoleApp.Testes
         {
             var agenda = new Agenda();
 
-            var contato = new Contato()
-            {
-                Nome = "Sergio",
-                Numero = 9891211
-            };
+            var contato = new Contato("Sergio", 12345);
+
 
             agenda.AdicionarContato(contato);
+
+            Assert.AreEqual(agenda.QuantidadeContatos, 1);
+        }
+
+        [TestMethod]
+        public void AgendaTemDoisContatosEÉRemovidoUmContatoPorNome()
+        {
+            var agenda = new Agenda();
+
+            var contato = new Contato("Sergio", 12345);
+            var contato1 = new Contato("Test", 12345);
+
+
+            agenda.AdicionarContato(contato);
+            agenda.AdicionarContato(contato1);
+
+            agenda.RemoverContatosPorNome("Test");
+
+
+            Assert.AreEqual(agenda.QuantidadeContatos, 1);
+        }
+
+        [TestMethod]
+        public void AgendaTemDoisContatosEÉRemovidoUmContatoPorNumero()
+        {
+            var agenda = new Agenda();
+
+            var contato = new Contato("Sergio", 12345);
+            var contato1 = new Contato("Test", 12345);
+
+
+            agenda.AdicionarContato(contato);
+            agenda.AdicionarContato(contato1);
+
+            agenda.RemoverContatosPorNumero(12345);
+
+
+            Assert.AreEqual(agenda.QuantidadeContatos, 1);
+        }
+
+        [TestMethod]
+        public void AgendaTem3ContatoEDoisSaoRemovidosPorNomeIgual()
+        {
+            var agenda = new Agenda();
+
+            agenda.AdicionarContato(new Contato("Sergio", 12345));
+            agenda.AdicionarContato(new Contato("Joao", 98766));
+            agenda.AdicionarContato(new Contato("Sergio", 123456));
+
+            agenda.RemoverContatosPorNome("Sergio");
+
+            Assert.AreEqual(agenda.QuantidadeContatos, 1);
+        }
+
+        [TestMethod]
+        public void AgendaTem3ContatoEDoisSaoRemovidosPorNumeroIgual()
+        {
+            var agenda = new Agenda();
+
+            agenda.AdicionarContato(new Contato("Sergio", 12345));
+            agenda.AdicionarContato(new Contato("Joao", 98766));
+            agenda.AdicionarContato(new Contato("Sergio", 12345));
+
+            agenda.RemoverContatosPorNumero(12345);
 
             Assert.AreEqual(agenda.QuantidadeContatos, 1);
         }
