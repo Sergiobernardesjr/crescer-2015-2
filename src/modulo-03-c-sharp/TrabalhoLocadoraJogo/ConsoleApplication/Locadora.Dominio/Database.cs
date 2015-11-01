97aed4071bc7ba4a84185912542a776268c7ef4d
Sergio Luiz Nunes Bernardes Junior <sergio.bernardes@hotmail.com.br>
2015-11-01 18:30:52 -0200

    criado o relatório, faltando tabulação e o cabeçalho final;

--- src/modulo-03-c-sharp/TrabalhoLocadoraJogo/ConsoleApplication/Locadora.Dominio/Database.cs
+++ src/modulo-03-c-sharp/TrabalhoLocadoraJogo/ConsoleApplication/Locadora.Dominio/Database.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,7 +11,7 @@ namespace Locadora.Dominio
 {
     public class Database
     {
-
+        
         public const string XML = @"C:\Users\Sergio\Documents\CWI\Crescer\Modulo_01\src\modulo-03-c-sharp\TrabalhoLocadoraJogo\ConsoleApplication\Database\Jogos.xml";
 
         public void CadastrarJogo(Jogo jogo)
@@ -71,6 +72,33 @@ namespace Locadora.Dominio
             }
 
             xmlJogos.Save(XML);
+        }
+
+        public void RelatorioJogos()
+        {
+            string caminhoRel = @"C:\Users\Sergio\Documents\CWI\Crescer\Modulo_01\src\modulo-03-c-sharp\TrabalhoLocadoraJogo\ConsoleApplication\Database\RelatorioJogos.txt";
+            XElement xmlJogos = XElement.Load(XML);
+
+            DateTime dataAtual = DateTime.Now;
+                                    
+            string cabecalhoNomeLocadora = "                             LOCADORA GAMES TCHE                               " + Environment.NewLine;
+            string dataEHoraRel = String.Format("{0:dd/MM/yyyy}                                                       {0:HH:mm:ss}", dataAtual);
+            string cabecalhoData = dataEHoraRel + Environment.NewLine;
+            string nomeRelatorio =         "                              Relatório de Jogos                                 " + Environment.NewLine;
+            string separacao =             "===============================================================================" + Environment.NewLine;
+            string itensDoCabecalho =      "ID\t       Categoria\t             Nome\t                     Preço              " + Environment.NewLine;
+                                                
+            File.AppendAllText(caminhoRel, cabecalhoNomeLocadora);
+            File.AppendAllText(caminhoRel, cabecalhoData);
+            File.AppendAllText(caminhoRel, nomeRelatorio);
+            File.AppendAllText(caminhoRel, separacao);
+            File.AppendAllText(caminhoRel, itensDoCabecalho);
+
+            foreach (var item in xmlJogos.Elements())
+            {
+                string fraseJogos = String.Format("{0}\t      {1}\t                   {2}\t                       {3}                ", item.Attribute("id").Value.ToString(), item.Element("categoria").Value.ToString(), item.Element("nome").Value.ToString(), item.Element("preco").Value.ToString()) + Environment.NewLine ;
+                File.AppendAllText(caminhoRel, fraseJogos);
+            }
         }
     }
 }
