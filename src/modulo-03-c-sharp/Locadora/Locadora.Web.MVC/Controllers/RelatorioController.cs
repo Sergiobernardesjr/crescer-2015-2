8209c856218374c355bb707e147bb9aff58ca005
Sergio Luiz Nunes Bernardes Junior <sergio.bernardes@hotmail.com.br>
2015-11-05 15:59:09 -0200

    Adicionado o relatório de jogos;

---
+++ src/modulo-03-c-sharp/Locadora/Locadora.Web.MVC/Controllers/RelatorioController.cs
@@ -0,0 +1,48 @@
+﻿using Locadora.Dominio.Repositorio;
+using Locadora.Web.MVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Locadora.Web.MVC.Controllers
+{
+    public class RelatorioController : Controller
+    {
+        private IJogoRepositorio repositorio = new Locadora.Repositorio.ADO.JogoRepositorio();
+
+        public ActionResult JogosDisponiveis()
+        {
+            RelatorioModel relatorio = new RelatorioModel();
+
+            var jogos = repositorio.BuscarTodos();
+
+            foreach (var jogo in jogos)
+            {
+                JogoModel jogoModel = new JogoModel()
+                {
+                    Nome = jogo.Nome,
+                    Categoria = jogo.Categoria.ToString(),
+                    Preco = jogo.Preco
+                };
+
+                relatorio.listaJogos.Add(jogoModel);
+            }
+
+            var jogoMaisCaro = relatorio.listaJogos.Max(jogo => jogo.Preco);
+            var jogoMaisBarato = relatorio.listaJogos.Min(jogo => jogo.Preco);
+
+            relatorio.nomeJogoMaiorPreco = relatorio.listaJogos.First(jogo => jogo.Preco == jogoMaisCaro).Nome;
+
+            relatorio.nomeJogoMenorPreco = relatorio.listaJogos.First(jogo => jogo.Preco == jogoMaisBarato).Nome;
+
+            relatorio.qtdDeJogos = relatorio.listaJogos.Count();
+
+            relatorio.mediaValorDosJogos = relatorio.listaJogos.Average(jogo => jogo.Preco);
+
+            return View(relatorio);
+        }
+
+    }
+}
---
+++ src/modulo-03-c-sharp/Locadora/Locadora.Web.MVC/Models/RelatorioModel.cs
@@ -0,0 +1,22 @@
+﻿using Locadora.Dominio.Repositorio;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Locadora.Web.MVC.Models
+{
+    public class RelatorioModel
+    {
+        public List<JogoModel> listaJogos = new List<JogoModel>();
+        public int qtdDeJogos { get; set; }
+        public string nomeJogoMaiorPreco { get; set; }
+        public string nomeJogoMenorPreco { get; set; }
+        public decimal mediaValorDosJogos { get; set; }
+
+        public RelatorioModel()
+        {
+            listaJogos = new List<JogoModel>();
+        }
+    }
+}
