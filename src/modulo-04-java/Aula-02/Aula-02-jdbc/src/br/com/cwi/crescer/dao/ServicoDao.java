630bd269192b452eb3700ab813f2c32ddd782f68
Sergio Luiz Nunes Bernardes Junior <sergio.bernardes@hotmail.com.br>
2015-11-17 17:41:11 -0200

    Adicionado o metodo de lsitar os servicos.

--- src/modulo-04-java/Aula-02/Aula-02-jdbc/src/br/com/cwi/crescer/dao/ServicoDao.java
+++ src/modulo-04-java/Aula-02/Aula-02-jdbc/src/br/com/cwi/crescer/dao/ServicoDao.java
@@ -2,7 +2,10 @@ package br.com.cwi.crescer.dao;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 import br.com.cwi.crescer.jdbc.ConnectionFactory;
 import br.com.cwi.crescer.model.Servico;
@@ -20,6 +23,26 @@ public class ServicoDao {
         } catch (SQLException e) {
             throw new SQLException(e);
         }
+    }
+
+    public List<Servico> listAll() throws SQLException {
+        List<Servico> lista = new ArrayList<Servico>();
+        try (Connection conexao = new ConnectionFactory().getConnection()) {
+            StringBuilder query = new StringBuilder();
+            query.append(" select idServico, dsServico from Servico ");
+            PreparedStatement statement = conexao.prepareStatement(query.toString());
+            ResultSet resultSet = statement.executeQuery();
+            while (resultSet.next()) {
+                Servico servico = new Servico();
+                servico.setIdServico(resultSet.getLong(1));
+                servico.setDsServico(resultSet.getString(2));
+                lista.add(servico);
+            }
+        } catch (SQLException e) {
+            throw e;
+        }
+
+        return lista;
     }
 
 }
